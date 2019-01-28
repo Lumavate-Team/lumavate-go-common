bb8bab906fcb56ed1e76776b48dcec8c7d001d62
Jack Eberhardt <j.eberhardt@lumavate.com>
2019-01-28 17:35:51 -0500

    help text modified and set lifespan default back to 0

--- components/addToHome.go
+++ components/addToHome.go
@@ -15,13 +15,13 @@ func GetAddToHomeProperties() [] properties.PropertyType {
 	    true})
 
   props = append(props, &properties.PropertyNumeric{
-      &properties.PropertyBase{"startDelay", "Add To Home Screen", "Prompt Time Configuration", "Delay Time", "Add to Home Screen Prompt will display X seconds after the page has fully loaded. If set to 0, the prompt will display on page load."}, 0, properties.PropertyOptionsNumeric{ Min: 0, Max: 32}})
+      &properties.PropertyBase{"startDelay", "Add To Home Screen", "Prompt Time Configuration", "Delay Time", "Add to Home Screen Prompt will display X seconds after the page has fully loaded. If set to 0, the prompt will display on page load."}, 0, properties.PropertyOptionsNumeric{ Min: 0, Max: 30}})
 
   props = append(props, &properties.PropertyNumeric{
-      &properties.PropertyBase{"lifespan", "Add To Home Screen", "Prompt Time Configuration", "Lifespan", "Lifespan is the duration of the prompt message defined in seconds. We recommend giving your users enough time to read your prompt message; max duration is 30 seconds."}, 1, properties.PropertyOptionsNumeric{ Min: 1, Max: 30}})
+      &properties.PropertyBase{"lifespan", "Add To Home Screen", "Prompt Time Configuration", "Lifespan", "Lifespan is the duration of the prompt message in seconds. We recommend giving your users enough time to read your prompt message; max duration is 30 seconds. If you would like the message to persist with no time expiration, set the duration to 0; (0 = infinity)."}, 0, properties.PropertyOptionsNumeric{ Min: 0, Max: 30}})
 
   props = append(props, &properties.PropertyNumeric{
-      &properties.PropertyBase{"displayCount", "Add To Home Screen", "Prompt Time Configuration", "Display Count", "Display Count is the number of days you would like this prompt to appear until a user saves the app to their home screen. By default, the prompt message will display once per day until the user saves the app to their home screen. If you would like the prompt message to display until action is taken, set Display Count to 0."}, 0, properties.PropertyOptionsNumeric{ Min: 0, Max: 32}})
+      &properties.PropertyBase{"displayCount", "Add To Home Screen", "Prompt Time Configuration", "Display Count", "Display Count is the number of days you would like this prompt to appear until a user saves the app to their home screen. By default, the prompt message will display once per day until the user saves the app to their home screen. If you would like the prompt message to display until action is taken, set Display Count to 0."}, 0, properties.PropertyOptionsNumeric{ Min: 0, Max: 30}})
 
   props = append(props, &properties.PropertyText{
     	&properties.PropertyBase{"message", "Add To Home Screen", "Prompt Message", "Message", "This is the message that will be displayed to your users. Your message should be concise and explain how to add the app to the home screen. The prompt message will display your fav icon—configured in the App Manifest on your Home Page."},
