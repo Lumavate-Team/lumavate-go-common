b324f3fc8cf22fd149795c8e85d089e4fdeafb8e
Eric Seidel <e.seidel@lumavate.com>
2020-04-29 18:34:46 +0000

    new models

--- models/app_settings.go
+++ models/app_settings.go
@@ -2,7 +2,6 @@
 package models
 
 type AppSettingsStruct struct {
-  PageType PageTypeStruct `json:"pageType"`
   Footer ComponentStruct `json:"footer"`
   Header ComponentStruct `json:"header"`
   Fab ComponentStruct `json:"fab"`
@@ -12,12 +11,6 @@ type AppSettingsStruct struct {
   // and add custom unmarshal to move these properties if possible(would break page builder though)
   BodyMaxWidth int `json:"bodyMaxWidth"`
   BodyMaxWidthStr string `json:"bodyMaxWidthStr"`
-  ShowAddToHome bool `json:"showAddToHome"`
-  SkipFirst bool `json:"skipFirst"`
-  StartDelay int `json:"startDelay"`
-  Lifespan int `json:"lifespan"`
-  DisplayCount int `json:"displayCount"`
-  Message string `json:"message"`
-
   StyleData [] Styles `json:"styleData"`
+  AddToHomeStruct
 }
