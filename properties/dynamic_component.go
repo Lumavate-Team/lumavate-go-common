bc0116a4b385ac2b8fb9bd2e869cc52dd205d3a3
Eric Seidel <e.seidel@lumavate.com>
2020-05-06 01:40:48 +0000

    SSW-15 tweaked dynamic component properties to closer align with propertyComponent

--- properties/dynamic_component.go
+++ properties/dynamic_component.go
@@ -6,10 +6,16 @@ import (
   "encoding/json"
 )
 
+type DynamicOptions struct {
+  ComponentTagName string `json:"componentTagName"`
+  AllowMultiple bool `json:"allowMultiple"`
+	Components [] *Component `json:"components"`
+}
 type DynamicComponent struct {
   *PropertyBase
   ComponentTagName string `json:"componentTagName"`
   AllowMultiple bool `json:"allowMultiple"`
+  Options DynamicOptions
 }
 
 func (p *DynamicComponent) MarshalJSON() (b []byte, e error) {
