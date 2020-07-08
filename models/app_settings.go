e6109d346dc371412738f819e5937d0bbdb8c3bc
Eric Seidel <e.seidel@lumavate.com>
2020-07-08 05:01:51 +0000

    PLAT3-25 changes for style data at branding level

--- models/app_settings.go
+++ models/app_settings.go
@@ -1,11 +1,12 @@
 package models
 
 type AppSettingsStruct struct {
+  ThemeDataStruct
   Header ComponentStruct `json:"header"`
   Footer ComponentStruct `json:"footer"`
   Fab ComponentStruct `json:"fab"`
 
+
   BodyMaxWidth int `json:"bodyMaxWidth"`
   BodyMaxWidthStr string `json:"bodyMaxWidthStr"`
-  ThemeDataStruct
 }
--- models/theme_data.go
+++ models/theme_data.go
@@ -1,14 +1,14 @@
 package models
 
-type ThemeDataStruct {
-  MainFontFamily `json:"mainFontFamily"`
-  PrimaryColor `json:"primaryColor"`
-  SecondaryColor `json:"secondaryColor"`
-  TertiaryColor `json:"tertiaryColor"`
-  AccentColor `json:"accentColor"`
-  SuccessColor `json:"successColor"`
-  AlertColor `json:"alertColor"`
-  DarkColor `json:"darkColor"`
-  MediumColor `json:"mediumColor"`
-  LightColor `json:"lightColor"`
+type ThemeDataStruct struct {
+  MainFontFamily string `json:"mainFontFamily"`
+  PrimaryColor string `json:"primaryColor"`
+  SecondaryColor string `json:"secondaryColor"`
+  TertiaryColor string `json:"tertiaryColor"`
+  AccentColor string `json:"accentColor"`
+  SuccessColor string `json:"successColor"`
+  AlertColor string `json:"alertColor"`
+  DarkColor string `json:"darkColor"`
+  MediumColor string `json:"mediumColor"`
+  LightColor string `json:"lightColor"`
 }
