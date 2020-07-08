0ff7dec32d6e6ccfbd850bc7341f62d8111d9c81
Eric Seidel <e.seidel@lumavate.com>
2020-07-08 16:58:27 +0000

    PLAT3-25 common functions moved here

--- common.go
+++ common.go
@@ -4,6 +4,8 @@ import (
 	models "github.com/Lumavate-Team/lumavate-go-common/models"
 	component_data "github.com/Lumavate-Team/lumavate-go-common/properties/component_data"
 	"html/template"
+  "strings"
+  "regexp"
 )
 
 func SafeCss(in string) (out template.CSS) {
@@ -14,6 +16,28 @@ func SafeCss(in string) (out template.CSS) {
 func SafeHtml(in string) (out template.HTML) {
 	out = template.HTML(in)
 	return
+}
+
+func HasSuffix(in string, test string) (out bool){
+  return strings.HasSuffix(in, test)
+}
+func HasPrefix(in string, test string) (out bool){
+  return strings.HasPrefix(in, test)
+}
+func Replace(input, from,to string) string {
+ \return strings.Replace(input,from,to, -1)
+}
+
+func Esm(src string)(out string){
+  extIndex := strings.LastIndex(src, ".js")
+  return src[:extIndex] + ".esm.js"
+}
+
+
+func EscapeSpecial(src string)string{
+  reg, _:= regexp.Compile("[^a-zA-Z0-9_-]+")
+  s := strings.Replace(src, " ", "_", -1)
+  return reg.ReplaceAllString(s, "")
 }
 
 func ComponentHtml(in component_data.ComponentData) (out template.HTML) {
