ad62cb5e318a6419443825ff9b35b95c33226a4d
Eric Seidel <e.seidel@lumavate.com>
2020-07-08 15:25:27 +0000

    PLAT3-25 branding data changes and layout

--- models/app_settings.go
+++ models/app_settings.go
@@ -1,12 +1,15 @@
 package models
 
 type AppSettingsStruct struct {
-  ThemeDataStruct
+  InstanceName      string                        `json:"instance__name"`
+  PageType          common_models.PageTypeStruct  `json:"pageType"`
+  DirectIncludes    [] string                     `json:"__directIncludes"`
+  DirectCssIncludes [] string                     `json:"__directCssIncludes"`
   Header ComponentStruct `json:"header"`
   Footer ComponentStruct `json:"footer"`
   Fab ComponentStruct `json:"fab"`
 
-
   BodyMaxWidth int `json:"bodyMaxWidth"`
   BodyMaxWidthStr string `json:"bodyMaxWidthStr"`
+  ThemeDataStruct
 }
