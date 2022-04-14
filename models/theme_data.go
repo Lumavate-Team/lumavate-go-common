f0405582f515134e460bb5cf15e9d28433083242
Eric Seidel <e.seidel@lumavate.com>
2022-04-14 15:54:03 -0400

    WAB-1 new theme styles

--- models/theme_data.go
+++ models/theme_data.go
@@ -2,6 +2,8 @@ package models
 
 type ThemeDataStruct struct {
   MainFontFamily string `json:"mainFontFamily"`
+  SecondaryFontFamily string `json:"secondaryFontFamily"`
+  TertiaryFontFamily string `json:"tertiaryFontFamily"`
   PrimaryColor string `json:"primaryColor"`
   SecondaryColor string `json:"secondaryColor"`
   TertiaryColor string `json:"tertiaryColor"`
@@ -11,4 +13,19 @@ type ThemeDataStruct struct {
   DarkColor string `json:"darkColor"`
   MediumColor string `json:"mediumColor"`
   LightColor string `json:"lightColor"`
+
+  Heading1FontStyle FontStyleStruct `json:"h1FontStyle"`
+  Heading2FontStyle FontStyleStruct `json:"h2FontStyle"`
+  Heading3FontStyle FontStyleStruct `json:"h3FontStyle"`
+  Heading4FontStyle FontStyleStruct `json:"h4FontStyle"`
+  ParagraphFontStyle FontStyleStruct `json:"paragraphFontStyle"`
+  LinkFontStyle FontStyleStruct `json:"linkFontStyle"`
+  ButtonFontStyle FontStyleStruct `json:"buttonFontStyle"`
+
+}
+
+type FontStyleStruct struct {
+  FontColor string `json:"fontColor"`
+  FontFamily string `json:"fontFamily"`
+
 }
