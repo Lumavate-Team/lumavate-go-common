cbd6fec170c843b007c41d7bf62ebf5813514337
Connor Flynn <c.flynn@lumavate.com>
2023-02-02 15:54:23 -0500

    PLAT3-2455: Adding dynamic property list to go properties

---
+++ properties/dynamic_property_list.go
@@ -0,0 +1,29 @@
+package properties
+
+import (
+  "encoding/json"
+)
+
+type DynamicPropertyOptions struct {
+	PropertyDef string `json:"propertyDef"`
+	OnSettings bool `json:"onSettings"`
+	Max int `json:"max"`
+	FullHeight bool `json:"fullHeight"`
+}
+
+type DynamicPropertyList struct {
+  *PropertyBase
+  Options DynamicPropertyOptions `json:"options"`
+}
+
+func (p *DynamicPropertyList) MarshalJSON() (b []byte, e error) {
+  type Copy DynamicPropertyList
+
+  return json.Marshal(&struct{
+		Type string `json:"type"`
+		*Copy
+	}{
+		"dynamic-property-list",
+		(*Copy)(p),
+	})
+}
