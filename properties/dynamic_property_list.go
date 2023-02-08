46c61ef8914194e815e708ac8a7299a605f982d9
Connor Flynn <c.flynn@lumavate.com>
2023-02-08 08:24:46 -0500

    PLAT3-2455: Reverting property def

--- properties/dynamic_property_list.go
+++ properties/dynamic_property_list.go
@@ -5,7 +5,7 @@ import (
 )
 
 type DynamicPropertyOptions struct {
-	PropertyDef []byte `json:"propertyDef"`
+	PropertyDef string `json:"propertyDef"`
 	OnSettings bool `json:"onSettings"`
 	Max int `json:"max"`
 	FullHeight bool `json:"fullHeight"`
