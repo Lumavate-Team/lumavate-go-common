801dc95cba40caff52e8b00e6e49a4514f709703
Connor Flynn <c.flynn@lumavate.com>
2023-02-21 10:57:29 -0500

    PLAT3-2455: Adding Data Source support (#20)
    * PLAT3-2455: Adding data source property for go properties
    
    * PLAT3-2455: Adding dynamic property list to go properties
    
    * PLAT3-2455: Chainging propertydef to be []byte
    
    * PLAT3-2455: Adding data sources to widget data
    
    * PLAT3-2455: Reverting property def
    
    * PLAT3-2455: Adding generic typed property for dynamic list
    
    * PLAT3-2455: Adding default value for dynamic property list
    
    * PLAT3-2455: Removing default property for data source
    
    * PLAT3-2461; forcing standard view for page builder so it doesn't attempt to load fields
    
    ---------
    
    Co-authored-by: Chris Abernathy <c.abernathy@lumavate.com>
    Co-authored-by: abernathyc <72395529+abernathyc@users.noreply.github.com>

---
+++ properties/dynamic_property_list.go
@@ -0,0 +1,31 @@
+package properties
+
+import (
+  "encoding/json"
+)
+
+type DynamicPropertyOptions struct {
+	PropertyDef TypePropertyBase `json:"propertyDef"`
+	OnSettings bool `json:"onSettings"`
+	Max int `json:"max"`
+	FullHeight bool `json:"fullHeight"`
+	UseStandardView bool `json:"useStandardView"`
+	Default map[string]string `json:"default"`
+}
+
+type DynamicPropertyList struct {
+  *PropertyBase
+  Options DynamicPropertyOptions `json:"options"`
+}
+
+func (p *DynamicPropertyList) MarshalJSON() (b []byte, e error) {
+  type Copy DynamicPropertyList
+
+  return json.Marshal(&struct{
+		Type string `json:"type"`
+		*Copy
+	}{
+		"dynamic-property-list",
+		(*Copy)(p),
+	})
+}
